84dddf6628badcb2b6374bc282f724ed9a01b5df
Fellipe Araujo Aleixo <1301182@ifrn.local>
2025-07-14 16:09:14 -0300

    Resultado da aula de 14 de julho de 2025 - APOO InfoWeb T2.

--- mysite/cpp/views.py
+++ mysite/cpp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Resposta, CorrecaoPorPar
 
-# Create your views here.
+class AvaliarRespostaView(View):
+    def get(self, request, *args, **kwargs):
+        id_resp = kwargs.get('id_resp', '')
+        resp = Resposta.objects.get(pk = id_resp)
+        contexto = {'resposta': resp}
+        return render(
+            request, 'cpp/avalia_resposta.html', contexto
+        )
+    def post(self, request, *args, **kwargs):
+        id_resp = kwargs.get('id_resp', '')
+        resp = Resposta.objects.get(pk = id_resp)
+        # TODO: substituir pelo id do aluno logado
+        id_aluno = 2
+        nota = request.POST.get('nota', '')
+        comentarios = request.POST.get('comentarios', '')
+        if (resp and id_aluno and nota):
+            cpp = CorrecaoPorPar(aluno_id=id_aluno, nota=nota, comentarios=comentarios)
+            cpp.save()
+            resp.set_correcao(cpp)
+            resp.save()
+        return redirect('/avaliacao/{}/'.format(resp.avaliacao.id))
