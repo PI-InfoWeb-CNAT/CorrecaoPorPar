99b94c1263223c4e27d9b6c2959d8b1410d69b60
Fellipe A. Aleixo <fellipe.aleixo@ifrn.edu.br>
2025-07-20 18:42:02 -0300

    Finalizando os ajustes para teste da aplicação.

--- mysite/cpp/urls.py
+++ mysite/cpp/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     ),
     path(
         'resposta/<int:id_resposta>/avalia/', 
-        views.AvaliaRespostaView.as_view(), name='avalia'
+        views.AvaliaRespostaView.as_view(), name='avaliar'
     ),
     path(
         'turma/<int:id_turma>/nova-avaliacao/',
-        views.NovaAvaliacaoView.as_view(), name='nova-avaliacao'
+        views.NovaAvaliacaoView.as_view(), name='criar-avaliacao'
     ),
     path(
         '', # INICIALMENTE a página inicial será a listagem geral 
